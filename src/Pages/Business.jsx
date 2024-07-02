76e89ada7b891b6d03bb593c599a3ae1623e87aa
Sneha Gupta <snehagupta7385@gmail.com>
2024-07-02 13:26:13 +0530

    another commit

--- src/Pages/Business.jsx
+++ src/Pages/Business.jsx
@@ -3,8 +3,8 @@ import hut from "../images/hut.png";
 function Business() {
   return (
     <>
+    <h1 className='text-[50px] text-[#006442] my-[3rem] font-bold text-center'>Volks Holistic Wellness Resort</h1>
     <img src={hut} className='w-[400px] my-[3rem] rounded-[20px] mx-auto h-[400px]' />
-    <h1>Volks</h1>
     </>
   )
 }
