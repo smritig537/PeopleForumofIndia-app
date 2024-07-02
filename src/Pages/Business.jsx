da0ca1b149048c8f8ed4fd12c996ba07f8fb18d6
Sneha Gupta <snehagupta7385@gmail.com>
2024-07-02 15:23:13 +0530

    fjkdj

--- src/Pages/Business.jsx
+++ src/Pages/Business.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import hut from "../images/hut.png";
+import logo from "../images/logo1.png";
 function Business() {
   return (
     <>
-    <h1 className='text-[50px] text-[#006442] my-[3rem] font-bold text-center'>Volks Holistic Wellness Resort</h1>
-    <img src={hut} className='w-[400px] my-[3rem] rounded-[20px] mx-auto h-[400px]' />
+    <h1 className='text-[50px] text-[#006442] mt-[3rem] font-bold text-center'>Volks Holistic Wellness Resort</h1>
+    <img src={hut} className='w-[400px] mt-[3rem] mb-[1rem] rounded-[20px] mx-auto h-[400px]' />
+    <h1 className='italic text-sm text-[#006442] mx-auto text-center'>Developed and owned by volks infrastructure private limited</h1>
+   <div className='flex mx-auto justify-center w-[45%] '>
+    <img src={logo} className='w-[100px] h-[100px] mt-[2rem] mx-auto' />
+    <h1 className='text-[40px] text-[#006442] mt-[3rem] font-bold '>Volks Infrastructure Private Limited</h1>
+   </div>
+   <h1 className='text-[#006442] w-[45%]   text-[18px] my-[3rem] mx-auto'>﻿
+
+<span className='font-semibold'>M/S.VOLKS INFRASTRUKTUR PRIVATE LIMITED (VIPL)</span>, is a Private Ltd. Company incorporated in 2017. The Companies authorized share capital is INR 10,00,000.00, and its paid-up capital is INR. 10,00,000.00. The Company is primarily focussing on Design Built, Own, Operate and Transfer (DBOOT)/OPEX (Operational Expense) Projects on Automobile, Energy, Heath & Water projects. The VIPL did due diligence an heath sector after the COVID-19 pandemic and found the benefits of Ayurveda. <br></br><br></br>
+M/s.<span className='font-semibold'> VIPL</span> , has proposed to set up a 250 Acre - Ayurveda Wellness Centre in the name of "_<span className='font-semibold'>VOLKS HOLISTIC WELLNESS RESORT_" (_at RAMESWARAM,TAMILNADU, INDIA).</span> The extent of 300 Acres with state of art Infrastructure which includes but not limited to roads, secured campus street lightings, potable water supply, irrigation, green belt, storm water, traffic & special amenities, solid waste transfer station, wastewater collection & treatment with recycling unit, fibre optics network and Smart Central Command Control Centre, the highly efficient wind turbine & generators to create Green Energy. Miyawaki Forest to border this development, transcending an arid region to environmentally pure & fertile region giving rise to dwelling of numerous flora & Fauna. <br></br><br></br>
+<span className='font-semibold'>M/s. CARNOUSTIE, Carnoustie Ayurveda Programmes</span> <br></br><br></br>
+Ayurveda & Yoga are ancient systems of belief which have gained global prominence among many as a mainstream lifestyle choice. Carnoustie espouses this belief to the fullest and offers a range of treatments to its guests. Carnoustie Ayurveda & Wellness Resort is an award winning luxury Ayurveda Resort, offers its guests holistic treatments, combining practices of Ayurveda as Carnoustie's Signature Ayurveda Programmes.
+Wellness Spa - Aryavaidyasala The resort houses a Spa (ARYAVAIDYASALA), practicing an ancient Indian mystique art of healing, the scientific art that brings the mind and body in harmony with Mother Nature; the science of Ayurveda. Derived from Sanskrit, the compound word Ayurveda, implies two connected ideas - 'the science of life' and 'the art of living'. This traditional health science has been in practice in the bed of herbs and spices, Kerala, for more than 5000 years. And today, individuals from around the world travel to this Ayurvedic healing hub, seeking freedom from their ailments. </h1>
+<div className='w-[30%] italic text-[#006442] text-center mx-auto mb-[5rem]'>
+Regd. Office: 15/22, Lawyer Jagannathan Street, Alandur, Chennai - 600 032. E-mail: info@volks.co.in Web: www.volks.co.in
+CIN: U28999TN2017PTC119100 GSTIN: 33AAGCV1390K1Z6
+</div>
     </>
   )
 }
