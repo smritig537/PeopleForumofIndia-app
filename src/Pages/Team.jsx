5719f5ad94b9919de950eba043ac0f1725516bf2
Sneha Gupta <snehagupta7385@gmail.com>
2024-07-03 12:40:53 +0530

    finished work

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -13,7 +13,7 @@ function Team() {
   return (
   <>
          <h2 className='text-[40px] text-[#006442] my-[3rem] font-bold text-center sm:text-[50px]'>Meet Our Team</h2>
-                <div className="flex flex-col mx-auto w-[70%]">
+                <div className="flex flex-col overflow-x-hidden mx-auto w-[70%]">
                     <div className="cont1 mb-[5rem]  mx-auto">
                          <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={swami} className='w-[100%] h-[80%] rounded-t-[30px] ' />
@@ -65,8 +65,7 @@ Buddarapu Venkateswara Rao
                     <div className="cont1 mx-auto  mb-[5rem]">
                          <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img66} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                 
+                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>               
 Rajesh Bhatter
                                    </h1>
                                    <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Social Activist & Business man</h1>
@@ -112,9 +111,8 @@ Pathankoth, Punjab</h1>
                     <div className="cont1 mx-auto  mb-[5rem]">
                          <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img44} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   
-Bhawna
+                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>                        
+                                      Bhawna
                                    </h1>
                                    <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Pathankoth, Punjab</h1>
                          </div>
