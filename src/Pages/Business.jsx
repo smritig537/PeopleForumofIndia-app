e6d200f68c3a5eb8ed3fc71cab89836c17be8f56
Sneha Gupta <snehagupta7385@gmail.com>
2024-07-03 12:31:10 +0530

    finished

--- src/Pages/Business.jsx
+++ src/Pages/Business.jsx
@@ -4,15 +4,14 @@ import logo from "../images/logo1.png";
 function Business() {
   return (
     <>
-    <h1 className='text-[50px] text-[#006442] mt-[3rem] font-bold text-center'>Volks Holistic Wellness Resort</h1>
-    <img src={hut} className='w-[400px] mt-[3rem] mb-[1rem] rounded-[20px] mx-auto h-[400px]' />
+    <h1 className='text-[40px] text-[#006442] mt-[3rem] font-bold text-center sm:text-[50px]'>Volks Holistic Wellness Resort</h1>
+    <img src={hut} className='mt-[3rem] mb-[1rem] rounded-[20px] mx-auto  w-[350px]  h-[350px] sm:w-[400px]  sm:h-[400px]' />
     <h1 className='italic text-sm text-[#006442] mx-auto text-center'>Developed and owned by volks infrastructure private limited</h1>
-   <div className='flex mx-auto justify-center w-[45%] '>
-    <img src={logo} className='w-[100px] h-[100px] mt-[2rem] mx-auto' />
-    <h1 className='text-[40px] text-[#006442] mt-[3rem] font-bold '>Volks Infrastructure Private Limited</h1>
+   <div className='flex mx-auto justify-center w-[85%] xl:w-[45%] '>
+    <img src={logo} className='w-[90px] h-[90px] mt-[2rem] mx-auto' />
+    <h1 className='text-[25px] text-[#006442] mt-[3rem] font-bold md:text-[35px]'>Volks Infrastructure Private Limited</h1>
    </div>
-   <h1 className='text-[#006442] w-[45%]   text-[18px] my-[3rem] mx-auto'>﻿
-
+   <h1 className='text-[#006442] w-[65%] text-[18px] my-[3rem] mx-auto l:w-[45%]'>﻿
 <span className='font-semibold'>M/S.VOLKS INFRASTRUKTUR PRIVATE LIMITED (VIPL)</span>, is a Private Ltd. Company incorporated in 2017. The Companies authorized share capital is INR 10,00,000.00, and its paid-up capital is INR. 10,00,000.00. The Company is primarily focussing on Design Built, Own, Operate and Transfer (DBOOT)/OPEX (Operational Expense) Projects on Automobile, Energy, Heath & Water projects. The VIPL did due diligence an heath sector after the COVID-19 pandemic and found the benefits of Ayurveda. <br></br><br></br>
 M/s.<span className='font-semibold'> VIPL</span> , has proposed to set up a 250 Acre - Ayurveda Wellness Centre in the name of "_<span className='font-semibold'>VOLKS HOLISTIC WELLNESS RESORT_" (_at RAMESWARAM,TAMILNADU, INDIA).</span> The extent of 300 Acres with state of art Infrastructure which includes but not limited to roads, secured campus street lightings, potable water supply, irrigation, green belt, storm water, traffic & special amenities, solid waste transfer station, wastewater collection & treatment with recycling unit, fibre optics network and Smart Central Command Control Centre, the highly efficient wind turbine & generators to create Green Energy. Miyawaki Forest to border this development, transcending an arid region to environmentally pure & fertile region giving rise to dwelling of numerous flora & Fauna. <br></br><br></br>
 <span className='font-semibold'>M/s. CARNOUSTIE, Carnoustie Ayurveda Programmes</span> <br></br><br></br>
--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import img11 from "../images/img11.png";
 import img22 from "../images/img22.png";
-import img33 from "../images/img33.png";
+import abc from "../images/abc.jpg";
 import img44 from "../images/img44.png";
 import img55 from "../images/img55.png";
 import img66 from "../images/img66.png";
@@ -12,10 +12,10 @@ import mam from "../images/mam.jpg";
 function Team() {
   return (
   <>
-         <h2 className='text-[50px] text-[#006442] my-[3rem] font-bold text-center'>Meet Our Team</h2>
+         <h2 className='text-[40px] text-[#006442] my-[3rem] font-bold text-center sm:text-[50px]'>Meet Our Team</h2>
                 <div className="flex flex-col mx-auto w-[70%]">
                     <div className="cont1 mb-[5rem]  mx-auto">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={swami} className='w-[100%] h-[80%] rounded-t-[30px] ' />
                                    <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
                                    Narayananand Giri Maharaj
@@ -23,41 +23,38 @@ function Team() {
                                    <h1  className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
                          </div>
                     </div>
-
                 <div className='flex flex-wrap'>
-
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img11} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   
-P. D. Dinakaran Premkumar
+                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>                      
+                                    P. D. Dinakaran Premkumar
                                    </h1>
                                    <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Former Chief Justice, Tamil Nadu</h1>
                          </div>
-                    </div>
-                 
-                    <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
-                                   <img src={img33} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   Narayananand Giri Maharaj
+                    </div>      
+                    <div className="cont1 mx-auto  mb-[5rem]"> 
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                                   <img src={abc} className='w-[100%] h-[80%] rounded-t-[30px] ' />
+                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>                          
+                                                                                                     John Joseph
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
+                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Former Retd, National GST Chairman</h1>
                          </div>
                     </div>
                    
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img55} className='w-[100%] h-[80%] rounded-t-[30px] ' />
                                    <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   Narayananand Giri Maharaj
+                                   
+Buddarapu Venkateswara Rao
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
+                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Former Judicial Member C.A.T, AP</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={mam} className='w-[100%] h-[80%] rounded-t-[30px] ' />
                                    <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
                                    Priya Nair
@@ -66,30 +63,33 @@ P. D. Dinakaran Premkumar
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img66} className='w-[100%] h-[80%] rounded-t-[30px] ' />
                                    <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   Narayananand Giri Maharaj
+                                 
+Rajesh Bhatter
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
+                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Social Activist & Business man</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img77} className='w-[100%] h-[80%] rounded-t-[30px] ' />
                                    <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   Narayananand Giri Maharaj
+                                   
+
+                                   Renu Gupta
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
+                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Social Activist, Haryana</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img88} className='w-[100%] h-[80%] rounded-t-[30px] ' />
                                    <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   Narayananand Giri Maharaj
+                                   Dhanraj Kella
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
+                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Ex MLA Surender Nagar, Gujarat</h1>
                          </div>
                     </div>
                 </div>
@@ -97,21 +97,26 @@ P. D. Dinakaran Premkumar
                 <h2 className='text-[50px] text-[#006442] my-[3rem] font-bold text-center'>Business Development Team</h2>
                     <div className='flex flex-wrap'>
                 <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img22} className='w-[100%] h-[80%] rounded-t-[30px] ' />
                                    <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   Narayananand Giri Maharaj
+                                  
+Surjit Singh Rana
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
+                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>
+
+
+Pathankoth, Punjab</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-[450px] w-[360px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col'>
+                         <div className='h-auto w-[310px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
                                    <img src={img44} className='w-[100%] h-[80%] rounded-t-[30px] ' />
                                    <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
-                                   Narayananand Giri Maharaj
+                                   
+Bhawna
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
+                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Pathankoth, Punjab</h1>
                          </div>
                     </div>
                     </div>
