a3f84014e4cb71b6233d7e6a325f58c15e04545c
Sneha Gupta <snehagupta7385@gmail.com>
2024-07-04 13:27:50 +0530

    team completed

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -20,146 +20,146 @@ function Team() {
          <h2 className='text-[40px] text-[#006442] my-[3rem] font-bold text-center sm:text-[50px]'>Meet Our Team</h2>
                 <div className="flex flex-col w-[70%] overflow-x-hidden mx-auto sm:w-[70%]">
                     <div className="cont1 mb-[5rem]  mx-auto">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className='w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={swami} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.1rem] text-[#006442] font-semibold sm:text-xl'>
                                    Narayananand Giri Maharaj
                                    </h1>
-                                   <h1  className='text-[#006442] text-[17px] m-[0.4rem]'>President</h1>
+                                   <h1  className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px] ml-[10px]'>President</h1>
                          </div>
                     </div>
                 <div className='flex flex-wrap'>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img11} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>                      
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>                      
                                     P. D. Dinakaran Premkumar
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Former Chief Justice, Tamil Nadu</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Former Chief Justice, Tamil Nadu</h1>
                          </div>
                     </div>      
                     <div className="cont1 mx-auto  mb-[5rem]"> 
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className='w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={abc} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>                          
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>                          
                                                                                                      John Joseph
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Former Retd, National GST Chairman</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Former Retd, National GST Chairman</h1>
                          </div>
                     </div>
                    
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img55} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                    
 Buddarapu Venkateswara Rao
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Former Judicial Member C.A.T, AP</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Former Judicial Member C.A.T, AP</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={mam} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                    Priya Nair
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Corporate International Coordinator</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Corporate International Coordinator</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className='w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img66} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>               
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>               
 Rajesh Bhatter
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Social Activist & Business man</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Social Activist & Business man</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img77} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                    
 
                                    Renu Gupta
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Social Activist, Haryana</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Social Activist, Haryana</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className='w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img111} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                   
 Chugh Rajan
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Investor & Business Man, Canada</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Investor & Business Man, Canada</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className='w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img222} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                    
 K C Kala
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Retd IRS, Maharashtra</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Retd IRS, Maharashtra</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img333} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                   
 Rajesh Tripathi
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Retd IRSE, Delhi</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Retd IRSE, Delhi</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img444} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                    Shri M.N. Krishnamurthy,IPS
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Ex DGP Kerala, Karnataka</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Ex DGP Kerala, Karnataka</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img555} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                    
 Mahesh Nautiyal
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Social Activist, Delhi</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Social Activist, Delhi</h1>
                          </div>
                     </div>
                 </div>
                 <div className='flex flex-col'>
-                <h2 className='text-[50px] text-[#006442] my-[3rem] font-bold text-center'>Business Development Team</h2>
+                <h2 className='text-[45px] text-[#006442] my-[3rem] font-bold text-center sm:text-[50px]'>Business Development Team</h2>
                     <div className='flex flex-wrap'>
                 <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
                                    <img src={img22} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>
                                   
 Surjit Singh Rana
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>
 
 
 Pathankoth, Punjab</h1>
                          </div>
                     </div>
                     <div className="cont1 mx-auto  mb-[5rem]">
-                         <div className='h-auto w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col sm:h-[450px] sm:w-[360px]'>
-                                   <img src={img44} className='w-[100%] h-[80%] rounded-t-[30px] ' />
-                                   <h1 className='text-xl m-[0.4rem] text-[#006442] font-semibold'>                        
+                         <div className=' w-[250px] border-[#006442] border-[1px] rounded-[30px]  flex flex-col h-[450px] sm:w-[360px]'>
+                                   <img src={img44} className='w-[100%] h-[80%] rounded-t-[30px]' />
+                                   <h1 className='text-lg m-[0.4rem] text-[#006442] font-semibold sm:text-xl'>                        
                                       Bhawna
                                    </h1>
-                                   <h1 className='text-[#006442] text-[17px] m-[0.4rem]'>Pathankoth, Punjab</h1>
+                                   <h1 className='text-[#006442] text-[15px] m-[0.4rem] sm:text-[17px]'>Pathankoth, Punjab</h1>
                          </div>
                     </div>
                     </div>
